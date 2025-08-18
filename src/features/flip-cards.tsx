e13eba28aa5cac686ee5cbbbb057ecf21f3cd755
kjndfahu <denispoda59@gmail.com>
2025-08-18 15:16:55 +0200

    changed padding

--- src/features/flip-cards.tsx
+++ src/features/flip-cards.tsx
@@ -91,7 +91,7 @@ export function FlipCards() {
 
   return (
     <section className="">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-20">
         <div className="text-center mb-25">
           <h2 className="text-3xl md:text-4xl font-bold text-black mb-4">
             Tradičné recepty našej tety Márgit
--- src/widgets/footer.tsx
+++ src/widgets/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="border-gray-200 mt-[80px] border-t-[1px] text-white">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="mx-auto px-4 sm:px-6 lg:px-20 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
             <div className="flex items-center mb-4">

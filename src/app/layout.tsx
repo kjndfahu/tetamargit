663b8a2e62f9110b092c9d0c621dadfc4c94bf0c
ruslan-mustafaev <therox5551998@gmail.com>
2025-09-20 15:42:48 +0200

    Fix favicon error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Teta Margit",
   description: "",
+  icons: {
+    icon: '/img/logo-teta.svg',
+  },
 };
 
 export default function RootLayout({

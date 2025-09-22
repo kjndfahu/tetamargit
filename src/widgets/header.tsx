81fc89941025ecd58118943502ddf23db27cac98
ruslan-mustafaev <therox5551998@gmail.com>
2025-09-22 20:05:00 +0200

    Updated header.tsx

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -27,7 +27,7 @@ export function Header() {
           <Link href="/">
             <div className="flex cursor-pointer items-center">
               <Image width={60} height={60} src="/img/logo-teta.svg" alt="logo" />
-              <span className="ml-3 text-2xl font-bold uppercase text-[#EE4C7C]">Teta Márgit</span>
+              <span className="ml-3 text-2xl font-bold uppercase text-[#EE4C7C]">111Teta Márgit</span>
             </div>
           </Link>
 

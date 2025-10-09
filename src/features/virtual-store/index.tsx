1a86aa0deb901973a0d0722b61f2ce0730cad25b
ruslan-mustafaev <therox5551998@gmail.com>
2025-10-09 12:10:33 +0200

    Updated index.tsx

--- src/features/virtual-store/index.tsx
+++ src/features/virtual-store/index.tsx
@@ -190,7 +190,7 @@ function VirtualStoreSectionComponent() {
   }
 
   return (
-    <section className="relative w-full min-h-[60vh] h-[70vh] md:h-[80vh] lg:h-screen overflow-hidden bg-gradient-to-b from-gray-900 to-gray-800">
+    <section className="relative w-full min-h-[50vh] h-[60vh] md:h-[70vh] lg:h-[85vh] overflow-hidden bg-gradient-to-b from-gray-900 to-gray-800">
       {/* 3D Canvas Container */}
       <div ref={containerRef} className="absolute inset-0" />
       
